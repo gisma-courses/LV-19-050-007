7c9f9196534dd5985133365edd56ba58a6e5ab06
gisma <gisma@users.noreply.github.com>
2023-06-26 08:23:44 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,10 @@
+moreaEventData = [
+  {title: "GIS in a Nutshell", url: "/LV-19-050-007/modules/module-prerequisites", start: "2021-10-26", end: "2021-11-02"},
+  {title: "Speed Dating GIS ", url: "/LV-19-050-007/morea/prerequisites/assessment.html", start: "2021-11-02T23:59"},
+  {title: "Change Detection Workflow", url: "/LV-19-050-007/modules/spatial-cd", start: "2022-01-23"},
+  {title: "Change Detection", url: "/LV-19-050-007/morea/change%20detection/assessment-cd.html", start: "2022-03-31T23:59"},
+  {title: "Aufgabe R-Spatial", url: "/LV-19-050-007/morea/geocomputation-with-r/assessment-spatial-r-gis-1.html", start: "2021-12-14T23:59"},
+  {title: "R-spatial - GIS mit R ", url: "/LV-19-050-007/modules/spatial-r-gis", start: "2021-11-23"},
+  {title: "Aufgabe Rmarkdown Seiten zur Dokumentation der Kursinhalte", url: "/LV-19-050-007/morea/gis-reproducible-research/assessment-gis-reproducible-research.html", start: "2021-11-16T23:59"},
+  {title: "Reproduzierbare Wissenschaft", url: "/LV-19-050-007/modules/gis-reproducible-research", start: "2021-12-11"}
+]
